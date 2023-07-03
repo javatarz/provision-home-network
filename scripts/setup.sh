eb01b4702c6055e85d920d49e5a93d1252dd41b7
Karun Japhet <karun@japhet.in>
2023-07-03 17:35:56 +0530

    Setup SSH key for BBB
    BBB starts off with a simple user login and ansible will not work with this.
    Host name should be read from the inventory file. The scripting for this can be improved later.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -25,5 +25,30 @@ echo
 echo "> Installing dependencies"
 pip install --upgrade pip -r ./scripts/requirements.txt 1>/dev/null
 
+remote_host="jetblade.local"
+echo
+echo "> Setting up SSH key for $remote_host"
+remote_port="22"
+remote_username="debian"
+local_ssh_key_path="$HOME/.ssh/keys/jetblade.rsa"
+
+if [[ -f "$local_ssh_key_path" ]]; then
+  echo ">> SSH key already exists locally and thus will not be installed remotely"
+else
+  echo ">> SSH key does not exist locally. Generating a new SSH key pair..."
+  ssh-keygen -t rsa -N '' -f "$local_ssh_key_path"
+  echo ">> SSH key pair generated."
+
+  ssh -p "$remote_port" "$remote_username"@"$remote_host" \
+  "mkdir -p ~/.ssh && \
+  echo \$(cat >> ~/.ssh/authorized_keys) && \
+  chmod 700 ~/.ssh && chmod 600 ~/.ssh/*" < "$local_ssh_key_path.pub"
+
+  echo ">> SSH key installed on the remote machine."
+fi
+echo ">> Writing fresh jetblade.config"
+rm ~/.ssh/envs/jetblade.config
+echo "Host jetblade.local\n  HostName jetblade.local\n  User debian\n  IdentityFile ~/.ssh/keys/jetblade.rsa" >> ~/.ssh/envs/jetblade.config
+
 echo
 echo "> Environment has been setup"
